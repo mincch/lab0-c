71d21de6e3ca4bb608e8329283822c8ca3a8d806
mincch <p76131563@gs.ncku.edu.tw>
2025-03-07 14:58:03 +0800

    Add function to delete the middle node
    use the slow and fast pointer approach to find the middle
    delete middle element by using 'list_del'
    
    Change-Id: If0cd172b60902e7003fab1ea55c23b0e635f57b2

--- queue.c
+++ queue.c
@@ -104,7 +104,18 @@ int q_size(struct list_head *head)
 /* Delete the middle node in queue */
 bool q_delete_mid(struct list_head *head)
 {
-    // https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
+    if (!head || list_empty(head))
+        return false;
+    struct list_head *slow = head->next;
+    struct list_head *fast = head->next;
+    while (fast != head && fast->next != head) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    list_del(slow);
+    element_t *del_node = list_entry(slow, element_t, list);
+    free(del_node);
+    free(del_node->value);
     return true;
 }
 
